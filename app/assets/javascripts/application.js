bc74af355882c3d33cc6b90c7305f9666ce005a6
codecademy <ccuser@codecademy.com>
2016-08-14 21:15:13 -0400

    eha commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,12 +12,15 @@
 );*/
 $('#button').click( 
     function() {
+      console.log("In the #button click");
       if ($(this).html() == "Menu") {
+        console.log("In the Menu");
         $('#right').animate({ left: 250 }, 'slow', function() {
             $('#button').html('Close');
         });
       }
       else {
+        console.log("In the else");
         $('#right').animate({ left: 0 }, 'slow', function() {
             $('#button').html('Menu');
         });
@@ -27,6 +30,7 @@ $('#button').click(
 
 $('#menu').click( 
     function() {
+      console.log("In the #menu click");
         $('#right').animate({ left: 0 }, 'slow', function() {
             $('#button').html('Menu');
         });
